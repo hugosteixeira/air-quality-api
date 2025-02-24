8f74fd2c4d40be4ca8f0a39498df9629f3dd546a
Hugo Teixeira <hugo.teixeira@dreamm.com.br>
2025-02-23 22:04:02 -0300

    update to use ssl

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from routers import data, watcher
 import threading
 from DataWatcher import DataWatcher
 from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
+import ssl
 
 
 app = FastAPI()
@@ -23,6 +24,9 @@ app.add_middleware(HTTPSRedirectMiddleware)
 app.include_router(data.router)
 app.include_router(watcher.router)
 
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+ssl_context.load_cert_chain('/etc/ssl/certs/apache-selfsigned.crt', keyfile='/etc/ssl/private/apache-selfsigned.key')
+
 def start_data_watcher():
     data_watcher = DataWatcher()
     data_watcher.start()
@@ -30,6 +34,4 @@ def start_data_watcher():
 if __name__ == "__main__":
     watcher_thread = threading.Thread(target=start_data_watcher)
     watcher_thread.start()
-    uvicorn.run(app, host="0.0.0.0",
-    ssl_keyfile='/etc/ssl/private/apache-selfsigned.key',
-    ssl_certfile='/etc/ssl/certs/apache-selfsigned.crt')
+    uvicorn.run(app, host="0.0.0.0", ssl=ssl_context)
