ad57fe46fd88feafac3c4c15f3d64583114c1809
Hugo Teixeira <hugo.teixeira@dreamm.com.br>
2025-02-23 21:56:30 -0300

    update to use ssl

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from routers import data, watcher
 import threading
 from DataWatcher import DataWatcher
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
+
 
 app = FastAPI()
 
@@ -15,6 +17,8 @@ app.add_middleware(
     allow_methods=["*"],  # Allow all methods
     allow_headers=["*"],  # Allow all headers
 )
+app.add_middleware(HTTPSRedirectMiddleware)
+
 
 app.include_router(data.router)
 app.include_router(watcher.router)
