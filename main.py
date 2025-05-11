5c6bfb14be4b41fdd1b5ef1519bcab21d54ab9b3
Hugo Teixeira <hugosteixeira@hotmail.com>
2025-05-11 18:56:36 -0300

    removes uvicorn run

--- main.py
+++ main.py
@@ -28,4 +28,4 @@ def start_data_watcher():
 if __name__ == "__main__":
     watcher_thread = threading.Thread(target=start_data_watcher)
     watcher_thread.start()
-    uvicorn.run(app, port="8000", host="0.0.0.0")
+    # uvicorn.run(app, port="8000", host="0.0.0.0")
